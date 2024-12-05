582415d6f7c257dc2985faf374373ee0fe22d12e
mingshuai.yuan <ymszs@qq.com>
2024-12-05 23:30:54 +0800

    fix: 拆分加载多个markdown文本

---
+++ src/app/test-doc/ItemContent.tsx
@@ -0,0 +1,54 @@
+import {useEffect, useRef} from "react";
+import {marked} from "marked";
+import katex from "katex";
+
+export default function ItemContent ({text}:{text: string}): JSX.Element {
+    const myRef = useRef(null);
+    /*  text = 'sss: \\[ \\text{间接费用} = 80 \\text{万元} \\times 20\\% = 16 \\text{万元} \\]'*/
+    // text = '$ \\text{平均数} = \\frac{\\text{总和}}{\\text{数量}} $ other code '
+    const htmlContent = marked.parse(
+        text
+    )
+    type Delimiter = {     left: string;     right: string;     display: boolean }
+    type Options = {
+        delimiters: Delimiter[]
+        [key: string]: unknown
+    }
+    const delimiters:Delimiter[] = [
+        {left: '\\$\\$', right: '\\$\\$', display: true},
+        {left: '\\$', right: '\\$', display: false},
+        {left: '\\[', right: '\\]', display: true},
+        {left: '\\(', right: '\\)', display: true},
+    ]
+    // 定义renderMathInElement函数，用于渲染元素中的LaTeX公式
+    function renderMathInElement(element: HTMLElement, options: Options) {
+        const delimiters = options.delimiters || [];
+        delimiters.forEach(delimiter => {
+            const regex = new RegExp(delimiter.left + '(.*?)' + delimiter.right, 'g');
+            element.innerHTML = element.innerHTML.replace(regex, (match, math) => {
+                const tex = math;
+                const node = document.createElement('span');
+                node.className = 'katex';
+                node.setAttribute('data-latex', tex);
+                options.output = 'html'
+                katex.render(tex, node, options as never);
+                return node.outerHTML;
+            });
+        });
+    }
+    useEffect(()=>{
+        // 使用KaTeX渲染LaTeX公式
+        renderMathInElement(myRef.current as unknown as HTMLElement, {delimiters})
+    })
+
+    return (<div>
+        <div ref={myRef} dangerouslySetInnerHTML={{__html: htmlContent}}></div>
+        {/*<br/>*/}
+        {/*<h2>html:</h2>*/}
+        {/*<div>{htmlContent}</div>*/}
+
+        {/*<br/>*/}
+        {/*<h2>md:</h2>*/}
+        {/*<div>{text}</div>*/}
+    </div>)
+}
--- src/app/test-doc/page.tsx
+++ src/app/test-doc/page.tsx
@@ -1,54 +1,24 @@
 'use client';
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 
-import {marked} from "marked";
-import { word } from "./word";
-import {useEffect, useRef} from "react";
-import katex from 'katex';
-export default function LinkGame() {
-    const myRef = useRef(null);
-    let text = word
-    // text = '$ \\text{平均数} = \\frac{\\text{总和}}{\\text{数量}} $ other code '
-   const htmlContent = marked.parse(
-       text
-    )
-    type Delimiter = {     left: string;     right: string;     display: boolean }
-    type Options = {
-        delimiters: Delimiter[]
-    }
-    const delimiters:Delimiter[] = [
-        {left: '\\$\\$', right: '\\$\\$', display: true},
-        {left: '\\$', right: '\\$', display: false},
-        {left: '\\[', right: '\\]', display: true},
-    ]
-    // 定义renderMathInElement函数，用于渲染元素中的LaTeX公式
-    function renderMathInElement(element: HTMLElement, options: Options) {
-        const delimiters = options.delimiters || [];
-        delimiters.forEach(delimiter => {
-            const regex = new RegExp(delimiter.left + '(.*?)' + delimiter.right, 'g');
-            element.innerHTML = element.innerHTML.replace(regex, (match, math) => {
-                const tex = math;
-                const node = document.createElement('span');
-                node.className = 'katex';
-                node.setAttribute('data-latex', tex);
-                katex.render(tex, node, options as never);
-                return node.outerHTML;
-            });
-        });
+import {useEffect, useState} from "react";
+
+import 'katex/dist/katex.min.css';
+import jsonData from './data.json';
+import ItemContent from "@/app/test-doc/ItemContent";
+export default function TestDoc() {
+    const [data, setData] = useState< any[]>([]);
+    async function getData(){
+        await fetch('./data.json')
+        setData(jsonData.data.chatItems)
     }
     useEffect(()=>{
-        console.log(myRef)
-        // 使用KaTeX渲染LaTeX公式
-        renderMathInElement(myRef.current as unknown as HTMLElement, {delimiters})
+        getData()
     },[])
     return (<div>
-        <div ref={myRef} dangerouslySetInnerHTML={{__html: htmlContent}}></div>
-        <br/>
-        <h2>html:</h2>
-        <div>{htmlContent}</div>
-
-        <br/>
-        <h2>md:</h2>
-        <div>{text}</div>
+        {data.map((item,idx) => <div key={idx}><ItemContent text={item.content}/>
+            <div>================================================================================================== <br /> ==================================================================================================
+            </div>
+        </div>)}
     </div>)
 }
