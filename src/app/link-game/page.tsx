004e1840f3a1fbec6916a694f1d726470e598017
mingshuai.yuan <ymszs@qq.com>
2024-12-08 23:38:57 +0800

    feat: 实现了单词块的判定功能

--- src/app/link-game/page.tsx
+++ src/app/link-game/page.tsx
@@ -3,9 +3,10 @@
 import TextBrick from "@/app/link-game/TextBrick";
 import {useEffect, useState} from "react";
 import {BrickState, WordInfo} from "@/app/link-game/typing";
+import {useImmer} from 'use-immer';
 
 export default function LinkGame() {
-    const [wordData,setWordData] = useState<WordInfo[]>([]);
+    const [wordData,updateWordData] = useImmer<WordInfo[]>([]);
     const getData = ()=>{
         const jsonData = [
             {id:'0',question:'你好',answer:'hello'},
@@ -16,19 +17,43 @@ export default function LinkGame() {
         const temp:typeof jsonData= []
         jsonData.forEach((item,idx)=>{
             temp[idx] = {...item}
-            temp[jsonData.length + idx] = {...item,id:item.id+'a'}
+            temp[jsonData.length + idx] = {...item,id:item.id+'_answer'}
         })
-        setWordData(temp)
+        updateWordData(temp)
     }
-    const onChooseWord = (idx: number)=>{
-        const newWordData = [...wordData];
-        if(newWordData[idx].state === BrickState.choose){
-            newWordData[idx].state = BrickState.normal
-        }else {
-            newWordData[idx].state = BrickState.choose;
-        }
-        setWordData(newWordData)
+    /* 选中的块id，用作两个块的比较 */
+    const [choseWordId, setChoseWordId] = useState<string | undefined>(undefined);
+    /* 获取单词块的类型 */
+    const getWordInfoType =  (id:string) =>{
+        return id.includes('_answer') ? 'answer' : 'question'
     }
+    /* 计算结果，比较两个不同类型的块，问题和答案对照正确则满足 */
+    const computeResult = (id1:string,id2:string)=> {
+        if(id1 === id2) return false;
+        return id1.replace('_answer','') === id2.replace('_answer','');
+    }
+    /* 选择单词块 */
+    const onChooseWord = (id: string) => {
+        updateWordData((draft) => {
+            let target = undefined as WordInfo | undefined;
+            let chooseWord = undefined as WordInfo | undefined;
+            draft.forEach(item => {
+                if(id === item.id) target = item
+                else if(choseWordId === item.id) chooseWord = item
+                else item.state = BrickState.normal
+
+            });
+            if (target && chooseWord && choseWordId && getWordInfoType(id) !== getWordInfoType(choseWordId)){
+                const result = computeResult(id,choseWordId)
+                target.state = chooseWord.state = result ? BrickState.success : BrickState.error;
+                setChoseWordId(undefined);
+            }else if(target){
+                target.state = target.state === BrickState.choose ? BrickState.normal : BrickState.choose;
+                if(chooseWord) chooseWord.state = BrickState.normal
+                setChoseWordId(target.state === BrickState.choose ? target.id : undefined);
+            }
+        });
+    };
     useEffect(()=>{
         console.log('onReady')
         getData()
@@ -37,13 +62,13 @@ export default function LinkGame() {
         <h1>Hello, Dashboard Page!</h1>
         <div className="flex justify-around">
             <div className="w-5/12 flex flex-col">
-                {wordData.slice(0,wordData.length/2).map((item,idx) =>{
-                    return (<TextBrick text={item.question} key={item.id} action={{state:item.state}} onChoose={()=>onChooseWord(idx)}></TextBrick>)
+                {wordData.slice(0,wordData.length/2).map((item) =>{
+                    return (<TextBrick text={item.question} key={item.id} action={{state:item.state}} onChoose={()=>onChooseWord(item.id)}></TextBrick>)
                 })}
             </div>
             <div className="w-5/12 flex flex-col">
-                {wordData.slice(wordData.length/2).map((item,idx) =>{
-                    return (<TextBrick text={item.answer} key={item.id}  action={{state:item.state}}  onChoose={()=>onChooseWord(wordData.length/2 + idx)}></TextBrick>)
+                {wordData.slice(wordData.length/2).map((item) =>{
+                    return (<TextBrick text={item.answer} key={item.id}  action={{state:item.state}}  onChoose={()=>onChooseWord(item.id)}></TextBrick>)
                 })}
             </div>
         </div>
